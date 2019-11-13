e23d2341f4fea4f9c9eeb32d33188524ec94f0b8
0andrejj0 <48532193+0andrejj0@users.noreply.github.com>
2019-11-13 22:42:17 +0300

    final commit
    bitcoding of tree

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <set>
 #include <cassert>
 
+
+
 const int N = 600;
 
 struct node{
@@ -35,6 +37,41 @@ void set_code(node *root, std::string s, std::vector<std::string> & code){
         set_code(root->l, s + '1', code);
         set_code(root->r, s + '0', code);
     }
+}
+
+unsigned char * to_code(unsigned char c, std::string & s){ // size =
+    std::cout << int(c) << ' ' << s.size() << ' ' << s << '\n';
+    int bytes = s.size() / 8 + !!s.size();
+    int bits = s.size();
+    unsigned char * l = new unsigned char[6];
+    for (int i = 0; i<6; i++)
+        l[i] = 0;
+    l[0] = c;
+    for (int bit = 0; bit < 16; ++bit)
+        if (bits & (1 << bit))
+            l[bit / 8 + 1] |= (1 << (bit % 8));
+    l[1] = bits;
+    l[2] = bits >> 8;
+    std::cout << bits << ' ' << (int)l[1] << ' ';
+    for (int bit = 0; bit < s.size(); ++bit){
+        if (s[bit] == '1')
+            l[bit/8 + 3] |= (1 << (bit % 8));
+    }
+    return l;
+}
+
+unsigned char * to_code(size_t n){ // 8 bytes
+    unsigned char * ans = new unsigned char [8];
+    for (int i = 0; i<8; i++)
+        ans[i] = 0;
+
+    ans[0] = n;
+    ans[1] = n >> 8;
+    ans[2] = n >> 16;
+    ans[3] = n >> 32;
+    ans[4] = n >> 40;
+    ans[5] = n >> 48;
+    return ans;
 }
 
 void compress(std::string name, std::string out_name){
@@ -73,17 +110,24 @@ void compress(std::string name, std::string out_name){
                 rcnt++;
             }
     }
-
     std::ofstream out(out_name, std::ios::out | std::ios::binary);
-    std::string num = std::to_string(rcnt);
-    out.write(num.c_str(), num.size());
-    out.write("\n", 1);
+    auto * ans = new unsigned char[8];
+    for (int i = 0; i<8; i++)
+        ans[i] = 0;
+    for (int bit = 0; bit < 50; ++bit){
+        int byte = bit/8;
+        if (rcnt & (1 << bit)){
+            ans[byte] |= (1 << (bit % 8));
+        }
+    }
+    std::cerr << rcnt << '\n';
+    out.write((char *)&ans[0], 8);
+
+
     for (int i = 0; i < 256; i++)
         if (cnt[i] != 0){
-            out.write(std::to_string(i).c_str(), std::to_string(i).size());
-            out.write(" ", 1);
-            out.write(code[i].c_str(), code[i].size());
-            out.write("\n", 1);
+            unsigned char * re = to_code(i, code[i]);
+            out.write((char *)re, 6);
         }
     int64_t code_len = 0;
     for (auto c:input)
@@ -93,7 +137,7 @@ void compress(std::string name, std::string out_name){
         code_len = (code_len + 8) - code_len % 8;
 
     code_len/=8;
-    auto * res = new char[code_len];
+    auto * res = new unsigned char[code_len];
     for (int i = 0; i < code_len; i++)
         res[i] = 0;
     int byte = 0;
@@ -108,75 +152,127 @@ void compress(std::string name, std::string out_name){
                 bit = 0;
             }
         }
-    std::string nums = std::to_string(input.size()) + ' ' + std::to_string(code_len) + '\n';
-    out.write(nums.c_str(), nums.size());
-    out.write(res, code_len);
+    auto a = to_code(input.size());
+    auto b = to_code(code_len);
+    out.write((char *)a, 8);
+    out.write((char *)b, 8);
+    out.write((char*)res, code_len);
+    std::cout << "ST: " << (int) res[0] << '\n';
 }
 
 void decompress(std::string name, std::string out_name){
     std::vector<std::string> code(256);
     std::ifstream fin;
-    fin.open(name);
-    int n;
-    fin >> n;
+    fin.open(name, std::ios::binary);
+    std::vector<unsigned char> input(std::istreambuf_iterator<char>(fin), {});
+    size_t n = 0;
+    for (int bit = 0; bit < 50; ++bit){
+        int byte = bit/8;
+        if (input[byte] & (1 << (bit % 8)))
+            n |= (1 << bit);
+    }
     node * root = new node;
     node * cur = root;
     for (int i = 0; i<n; i++){
-        int val;
-        fin >> val;
-        fin >> code[val];
+     std::vector<unsigned char> v(6);
+     int st = 6 * i + 8;
+     for (int byte = 0; byte < 8; ++byte)
+         v[byte] = input[st + byte];
+     std::cout << (int) v[1] << ' ';
+     int ch = v[0];
+     int len = 0;
+     for (int bit = 0; bit < 16; ++bit)
+         if (v[1 + bit/8] & (1 << ((bit) % 8)))
+            len |= (1 << bit);
+     std::string res_code;
+     for (int bit = 0; bit < len; ++bit){
+         int byte = bit / 8 + 3;
+         int b = bit % 8;
+         if (v[byte] & (1 << b))
+             res_code += '1';
+         else
+             res_code += '0';
+     }
+     std::cout << (int) ch <<' ' << len <<  ' ' << res_code << '\n';
+     code[ch] = res_code;
     }
-    for (int i = 0; i<256; i++)
-        if (!code[i].empty()){
-            for (auto c:code[i]){
-                if (c == '1'){
-                    if (cur -> l == nullptr)
-                        cur -> l = new node;
-                    cur = cur -> l;
-                }
-                if (c == '0'){
-                    if (cur -> r == nullptr)
-                        cur -> r = new node;
-                    cur = cur -> r;
-                }
-            }
-            cur -> value = i;
-            cur -> term = true;
-            cur = root;
+
+    uint64_t ans_size = 0, code_len = 0;
+    {
+        uint64_t st = 8 + 6 * n;
+        std::vector<unsigned char> a(8);
+        std::vector<unsigned char> b(8);
+
+        for (uint64_t i = 0; i < 8; i++)
+            a[i] = input[i + st];
+        for (uint64_t i = 0; i < 8; i++)
+            b[i] = input[i + st + 8];
+
+        for (uint64_t bit = 0; bit < 50; ++bit){
+            uint64_t byte = bit/8;
+            uint64_t B = bit % 8;
+            if (a[byte] & ((uint64_t)1 << B))
+                ans_size |= ((uint64_t)1 << bit);
         }
-    int ans_size, code_len;
-    fin >> ans_size >> code_len;
-    fin.close();
-    fin.open(name, std::ios::binary);
-    std::vector<unsigned char> input(std::istreambuf_iterator<char>(fin), {});
-    std::ofstream out(out_name, std::ios::out | std::ios::binary);
-    int stpos = 0;
-    int enter_cnt = 0;
-    for (stpos =  0; enter_cnt < n + 2; stpos++)
-        enter_cnt += (input[stpos] == '\n');
-    int byte = 0;
-    int bit = 0;
-    cur = root;
-    auto * ans = new unsigned char[ans_size];
-    int cur_pos = 0;
-    for (byte = stpos; byte<input.size() && cur_pos < ans_size; byte++){
-        for (bit = 0; bit < 8 && cur_pos < ans_size; bit++){
-            if (input[byte] & (1 << bit)){
-                cur = cur -> l;
-            } else
-                cur = cur -> r;
-            if (cur -> term){
-                ans[cur_pos++] = cur -> value;
-                cur = root;
-            }
+        for (uint64_t bit = 0; bit < 50; ++bit){
+            uint64_t byte = bit/8;
+            uint64_t B = bit % 8;
+            if (b[byte] & ((uint64_t)1 << B))
+                code_len |= ((uint64_t)1 << bit);
         }
     }
-    std::cerr << cur_pos << '\n';
-    out.write((char *)&ans[0], ans_size);
+
+    assert(ans_size < 1e8);
+
+     for (int i = 0; i<256; i++)
+         if (!code[i].empty()){
+             for (auto c:code[i]){
+                 if (c == '1'){
+                     if (cur -> l == nullptr)
+                         cur -> l = new node;
+                     cur = cur -> l;
+                 }
+                 if (c == '0'){
+                     if (cur -> r == nullptr)
+                         cur -> r = new node;
+                     cur = cur -> r;
+                 }
+             }
+             cur -> value = i;
+             cur -> term = true;
+             cur = root;
+         }
+
+     std::ofstream out(out_name, std::ios::out | std::ios::binary);
+     int stpos = 24 + n * 6;
+     std::cout << "ST: " << (int) input[stpos] << '\n';
+
+     int byte = 0;
+     int bit = 0;
+     cur = root;
+     auto * ans = new unsigned char[ans_size];
+     for (int i = 0; i < ans_size; i++)
+         ans[i] = 0;
+     int cur_pos = 0;
+     for (byte = stpos; byte<input.size() && cur_pos < ans_size; byte++){
+         for (bit = 0; bit < 8 && cur_pos < ans_size; bit++){
+             if (input[byte] & (1 << bit)){
+                 cur = cur -> l;
+             } else
+                 cur = cur -> r;
+             if (cur -> term){
+                 std::cout << cur -> value;
+                 ans[cur_pos++] = cur -> value;
+                 cur = root;
+             }
+         }
+     }
+     std::cerr << cur_pos << '\n';
+     out.write((char *)&ans[0], ans_size);
 }
 
 int main() {
-    compress("test.png", "test.myrar");
-    decompress("test.myrar", "ans.png");
+    compress("test.jpg", "test.myrar");
+    decompress("test.myrar", "ans.jpg");
     return 0;
 }
